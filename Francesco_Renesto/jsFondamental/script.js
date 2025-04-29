0f5a7d3c42cdbb0f0b0bfba9eebf78cb09094d8c
xRennyx <150335643+xRennyx@users.noreply.github.com>
2025-04-29 12:24:22 +0200

    Add files via upload

--- Francesco_Renesto/jsFondamental/script.js
+++ Francesco_Renesto/jsFondamental/script.js
@@ -37,9 +37,54 @@ array.push("Alfredo");
 console.log(array);
 array.pop();
 console.log(array);
-function myFunction(val1, val2)//passa per valore le variabili vengono duplicate
+function myFunction(val1, val2, array)//passa per valore le variabili vengono duplicate
 {                              //solo primitivi, array riferimento 
     console.log(val1+val2);
+    array.pop();
 }
-myFunction(10,20);
+myFunction(10,20, array);
 array.forEach(myFunction);
+function priNum(...args)//rest operator
+{
+    console.log(args);
+}
+priNum(2,3,4);
+priNum(3,4,5,6,7,8,9);
+
+let addTwoNum=function(a,b)
+{
+    return a+b;
+}
+console.log(addTwoNum(5,2));
+let addThreeNum=(a,b,c)=>a+b+c;//arrow function => 1 operazione
+console.log(addThreeNum(5,2,1));
+array.forEach(function(value, index,array2)
+{
+    array2[index]=value.toLowerCase();
+});
+console.log(array);
+
+let array3= array.filter(nome=>nome.startsWith('g'));
+console.log(array3);
+let saluti=array.map(nome=> `Ciao ${nome}`);
+console.log(saluti);
+mynum=[2,4,6,8,10];
+found= mynum.find(element=>element>5);
+console.log(found);
+num=mynum.reduce((accumulatore,numero)=>{
+return accumulatore+numero;
+},0);
+console.log(num);
+/*OGGETTO*/
+
+//coppia key-valore
+const persona={
+    nome: 'Antonio',
+    età: 25,
+    studente: false,
+    saluta: function(num){
+        console.log(`Ciao sono ${this.nome}, e il mio num preferito è ${num}`);
+    }
+};
+console.log(persona.nome);
+persona.saluta(10);
